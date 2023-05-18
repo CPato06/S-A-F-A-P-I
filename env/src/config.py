93fd199872e5718b5aa4308670eb02371b4c8936
Cpato06 <guillermo.patino@uabc.edu.mx>
2023-05-17 23:44:35 -0700

    prueba

---
+++ env/src/config.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+ 
+class DevelopmentConfig():
+     DEBUG=True
+     MYSQL_HOST='localhost'
+     MYSQL_USER='root'
+     MYSQL_PASSWORD=''
+     MYSQL_DB='solteck_beta1'
+     
+config={
+        'development': DevelopmentConfig
+        }
