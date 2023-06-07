fe07e2b7c8e7beb196e2b8302fef3457e34433ba
Carlos Patino <guillermo.patino@uabc.edu.mx>
2023-06-07 10:47:11 -0700

    d dsd

--- YonkeUniversal V1.0/conexion.php
+++ YonkeUniversal V1.0/conexion.php
@@ -1,6 +1,6 @@
 <?php
 
-    $servidor = "10.20.3.179";
+    $servidor = "database-api.cdjnyiihv9g0.us-east-2.rds.amazonaws.com";
     $usuario = "admin";
   	$contrasena = "Adminpato";
   	$dbnombre = "tienda";
